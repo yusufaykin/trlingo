4b40aae2bba9510c0d393ff5cd7bca0abe7d399b
yusufaykin <aykinyusuf1@gmail.com>
2024-08-10 18:45:17 +0300

    Initial Commit

---
+++ LangApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LangApp
+//
+//  Created by Yusuf on 10.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LangApp/LangAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangAppApp.swift
+//  LangApp
+//
+//  Created by Yusuf on 10.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LangAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
