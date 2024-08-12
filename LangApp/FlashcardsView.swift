ec5648ea1773574b92a424a20d93581992d1aaae
yusufaykin <aykinyusuf1@gmail.com>
2024-08-12 09:43:58 +0300

    Home page, word list and flash card pages have been created.

---
+++ LangApp/FlashcardsView.swift
@@ -0,0 +1,8 @@
+//
+//  FlashcardsView.swift
+//  LangApp
+//
+//  Created by Yusuf on 12.08.2024.
+//
+
+import Foundation
---
+++ LangApp/WordListView.swift
@@ -0,0 +1,8 @@
+//
+//  WordListView.swift
+//  LangApp
+//
+//  Created by Yusuf on 10.08.2024.
+//
+
+import Foundation
